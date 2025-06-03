45ac3a1f4af22d99a5c4c2e5640364f92efe929c
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2025-06-03 13:16:05 +0530

    Make the Blog-page

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,17 +1,20 @@
 import { assets, blog_data } from "@/Assets/assets";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const BlogItem = ({ title, description, image, category }) => {
+const BlogItem = ({ title, description, image, category, id }) => {
   return (
     <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]">
-      <Image
-        alt=""
-        src={image}
-        width={400}
-        height={400}
-        className="border-b border-black"
-      />
+      <Link href={`/blogs/${id}`}>
+        <Image
+          alt=""
+          src={image}
+          width={400}
+          height={400}
+          className="border-b border-black"
+        />
+      </Link>
       <p className="ml-5 mt-5 px-1 inline-block bg-black text-white text-sm ">
         {category}
       </p>
@@ -22,10 +25,13 @@ const BlogItem = ({ title, description, image, category }) => {
         <p className="mb-3 text-sm tracking-tight text-gray-700">
           {description}
         </p>
-        <div className="inline-flex items-center py-2 font-semibold text-center ">
+        <Link
+          href={`/blogs/${id}`}
+          className="inline-flex items-center py-2 font-semibold text-center "
+        >
           Read more
           <Image alt="" src={assets.arrow} width={12} className="ml-2" />
-        </div>
+        </Link>
       </div>
     </div>
   );
